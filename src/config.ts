f13f2fe84bf5f8de0ba19f645d8bc1abc449146f
Lbf <liubofan65@gmail.com>
2026-07-24 10:39:54 +0800

    Update profile avatar image path

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/evander.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Evander Liu",
 	bio: "Constant Learning",
 	links: [
